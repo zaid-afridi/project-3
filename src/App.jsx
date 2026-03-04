e7eaa2ea658750cd2fac3390d2146c7563648c7a
zaid-afridi <zaidafridi6868@gmail.com>
2026-03-04 11:04:58 +0500

    Add third project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='text-4xl bg-blue-400 w-full h-screen'>hadi
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
